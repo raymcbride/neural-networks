b53f77aaa2aa9026d0f46db693ac007acef6f249
Ray <ray@raymc>
2015-04-28 12:31:32 +0100

    converted to use maven

---
+++ src/main/java/com/raymcbride/neural/XMLParser.java
@@ -0,0 +1,45 @@
+package com.raymcbride.neural;
+
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.io.*;
+
+/**
+ * The XMLParser class is used to parse XML documents and create W3C DOM objects
+ *
+ *
+ * @author Ray McBride
+ */
+public class XMLParser{
+
+	private DocumentBuilderFactory factory;
+	private DocumentBuilder documentBuilder;
+	private Document document;
+
+	/**
+	 * This constructor for the <code>XMLParser</code>builds a new <code>Document</code> from
+	 * an XML file
+	 *
+	 * @param location The location of the XML file
+	 */
+	public XMLParser(String location){
+
+		try{
+			factory = DocumentBuilderFactory.newInstance();
+			documentBuilder = factory.newDocumentBuilder();
+			document = documentBuilder.parse(new File(location));
+		}
+		catch(Exception e){
+			System.out.println(e.toString());
+		}
+	}
+
+	/**
+	 * Gets the Document
+	 *
+	 * @return a Document
+	 */
+	public Document getDocument(){
+		return document;
+	}
+}
